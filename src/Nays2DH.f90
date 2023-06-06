e248f032db66e399f751d9a3292bfd23e583469e
Toshiki Iwasaki <Toshiki Iwasaki@TIWASAKI2021>
2023-06-06 16:44:26 +0900

    植生抗力係数0.7の根拠が不明なので，1へ変更 コンソール画面に出てくる文言をv4用に修正

--- src/Nays2DH.f90
+++ src/Nays2DH.f90
@@ -10361,8 +10361,8 @@ Program Shimizu
      tmp_caption(ii) = tmp_dummy
   end do
   !
-  write(*,*) 'Nays2DH on iRIC 3.x'
-  write(*,*) 'Copyright(C)2003-2018 by Yasuyuki SHIMIZU, Hokkaido Univ., Japan, and Hiroshi TAKEBAYASHI, Kyoto Univ., Japan, All Right Reserved'
+  write(*,*) 'Nays2DH on iRIC 4.x'
+  write(*,*) 'Copyright(C)2003-2023 by Yasuyuki SHIMIZU, Hokkaido Univ., Japan, and Hiroshi TAKEBAYASHI, Kyoto Univ., Japan, All Right Reserved'
   !
   !
   g     = 9.81d0
